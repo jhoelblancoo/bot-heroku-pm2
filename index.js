f60b88fe76897a7a5b491223104c12f26489324f
Jhoel Blanco <jhoelanexo@gmail.com>
2022-08-14 13:04:52 -0400

    prueba

---
+++ index.js
@@ -0,0 +1,18 @@
+const Telegraf = require("telegraf");
+const dotenv = require("dotenv").config();
+
+function main() {
+    //if (ctx.from){
+    const bot = new Telegraf(process.env.TOKEN);
+
+    bot.start(ctx => ctx.reply("Bienvenidos al  Bot de nskdj!"));
+
+    bot.hears("hola" || "Hola", ctx => {
+        ctx.reply("pruebaaaaa de pm2");
+        console.log(ctx.from.id);
+    });
+
+    bot.launch();
+}
+
+main();
