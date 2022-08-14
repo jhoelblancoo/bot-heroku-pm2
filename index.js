cf4aae9e399a2cfbc7b3e73072c9fb92ad3f050a
Jhoel Blanco <jhoelanexo@gmail.com>
2022-08-14 14:07:14 -0400

    nose

--- index.js
+++ index.js
@@ -1,20 +1,12 @@
-const io = require("@pm2/io");
-
-io.init({
-    metrics: {
-        network: {
-            ports: true,
-        },
-    },
-});
-
 const { Telegraf } = require("telegraf");
 const dotenv = require("dotenv").config();
 
 function main() {
+    console.log("first");
     //if (ctx.from){
     const bot = new Telegraf(process.env.TOKEN);
 
+    console.log(bot);
     bot.start(ctx => ctx.reply("Bienvenidos al  Bot de nskdj!"));
 
     bot.hears("hola" || "Hola", ctx => {
