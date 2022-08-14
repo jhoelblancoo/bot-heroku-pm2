93925f6bbd9be15961b279a3442e36e7c416c0f5
Jhoel Blanco <jhoelanexo@gmail.com>
2022-08-14 14:12:37 -0400

    rfgrreg

--- index.js
+++ index.js
@@ -2,15 +2,18 @@ const { Telegraf } = require("telegraf");
 const dotenv = require("dotenv").config();
 
 function main() {
-    console.log("first");
     //if (ctx.from){
     const bot = new Telegraf(process.env.TOKEN);
 
-    console.log(bot);
     bot.start(ctx => ctx.reply("Bienvenidos al  Bot de nskdj!"));
 
-    bot.hears("hola" || "Hola", ctx => {
-        ctx.reply("pruebaaaaa de pm2");
+    bot.hears("hola", ctx => {
+        ctx.reply("Hola estimado usuario!");
+        console.log(ctx.from.id);
+    });
+
+    bot.hears("chao", ctx => {
+        ctx.reply("chao estimado usuario!");
         console.log(ctx.from.id);
     });
 
